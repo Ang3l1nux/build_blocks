20c1b0d687c401d13dcde8ee666124cab6667517
Angelo Moreira <39745470+Ang3l1nux@users.noreply.github.com>
2020-07-11 18:44:57 -0300

    Update vpc_subnets.tf

--- vpc/vpc_subnets.tf
+++ vpc/vpc_subnets.tf
@@ -58,10 +58,10 @@ resource "aws_subnet" "private-subnet" {
 resource "aws_route_table" "rt_private" {
   vpc_id = "${aws_vpc.main.id}"
 
-  # route {
-  #   cidr_block = "0.0.0.0/0"
-  #   gateway_id = "${aws_nat_gateway.natgw.id}"
-  # }
+   route {
+     cidr_block = "0.0.0.0/0"
+     gateway_id = "${aws_nat_gateway.natgw.id}"
+   }
 
   tags = {
     Name    = "private_subnets"
